6471b11de136d1946905eb523c644ffa667f8798
branquitoRaph <raphaelbranco2004@gmail.com>
2022-10-10 10:10:21 -0300

    porta330

--- database.php
+++ database.php
@@ -5,7 +5,7 @@
 		public static function getInstance(){
 			if (!isset(self::$instance)){
 				try{
-					self::$instance = new PDO('mysql:host=' . HOST . ';port=3307;dbname=' . DB, USUARIO, SENHA);
+					self::$instance = new PDO('mysql:host=' . HOST . ';port=330;dbname=' . DB, USUARIO, SENHA);
 					self::$instance->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 					self::$instance->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
 				}catch(PDOException $e){
